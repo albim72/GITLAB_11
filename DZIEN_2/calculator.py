9655ce07f70458076d5aef2b9ee478a91472733a
albim72 <marcin.albiniak72@gmail.com>
2025-11-25 11:22:08 +0100

    Create calculator.py

---
+++ DZIEN_2/calculator.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> format:
+    return a+b
+
+def sub(a: float, b: float) -> format:
+    return a-b
+
+
+if __name__ == "__main__":
+    x = 2
+    y = 3
+    print(f"{x} + {y} = {add(x,y)}")
