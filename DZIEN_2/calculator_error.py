b3845957652e341f4523c4cb8e04b6f3e873e3b1
albim72 <marcin.albiniak72@gmail.com>
2025-11-25 13:10:37 +0100

    Implement basic add and subtract functions
    Fix function definitions and return statements in calculator_error.py.

---
+++ DZIEN_2/calculator_error.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> format:
+    return a+b+1
+
+def sub(a: float, b: float) -> format:
+    return a-b
+
+
+if __name__ == "__main__":
+    x = 2
+    y = 3
+    print(f"{x} + {y} = {add(x,y)}")
